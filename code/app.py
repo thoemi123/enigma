385beb2db83c1f386f3cf399c7724cd46d2d9ad8
thoemi123 <thomas.mendelin@hotmail.ch>
2022-07-27 22:16:54 +0200

    added tweet sentiment graph

--- code/app.py
+++ code/app.py
@@ -1,9 +1,10 @@
 import random
+from typing import Tuple
 
 
 import dash
 from dash import html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 from sqlalchemy import create_engine
@@ -18,30 +19,55 @@ from tickers.get_tickers import get_ticker, load_api_keys, convert_to_frame
 ENGINE = create_engine("sqlite:///app_db.sqlite")
 
 
-def generate_table(dataframe, max_rows=26):
+def generate_table(dataframe: pd.DataFrame, max_rows=26) -> html.Table:
+    """
+    Transform dataframe into html table
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+
+    max_rows : int, optional (default=26)
+
+    Returns
+    -------
+        html table 
+    """
     return html.Table(
-        # Header
         [html.Tr([html.Th(col) for col in dataframe.columns])] +
-        # Body
         [html.Tr([
             html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
 
-def get_sp_500_list(engine):
+def get_sp_500_list(engine) -> pd.Series:
+    """
+    select avaiable list of companies in data
+    """
     frame = pd.read_sql("select distinct matched_company from news_table", engine)
     return sorted(frame.matched_company.unique())
 
 
 def _get_dropdown_options(engine):
+    # create dropdown menu input
     company_names = get_sp_500_list(engine)
     return [
         {"label": c_name, "value": c_name} for c_name in company_names
     ]
 
 
-def calc_count_frames(selected_company):
+def calc_count_frames(selected_company: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Calculate counts of news article per companie and keywords
+
+    Parameters
+    ----------
+    selected_company : str
+    
+    Returns:
+        Two dataframes containing keywords and count numbers
+    """
     news_df = pd.read_sql(
         """
         select [date], matched_company, title
@@ -80,11 +106,22 @@ def calc_sentiment_frame(selected_company):
         """,
         ENGINE
     )
+
     news_df.date = pd.to_datetime(news_df.date)
     filtered_df = news_df[news_df.matched_company == selected_company]
 
     gics_sector = filtered_df.iloc[0].gics_sector
     sector_df = news_df[news_df.gics_sector == gics_sector]
+
+    tweet_df = pd.read_sql(
+        f"""
+        select
+            date,
+            sentiment_tweet
+        from tweet_table where sector = '{gics_sector}'
+        """, ENGINE
+    )
+    tweet_df.date = pd.to_datetime(tweet_df.date)
 
     plot_df = filtered_df.groupby(
         [pd.Grouper(key='date', freq='W-MON')]
@@ -98,9 +135,17 @@ def calc_sentiment_frame(selected_company):
         {"sentiment_news": "mean"}
     )
 
+    tweet_df = tweet_df.groupby(
+        [pd.Grouper(key='date', freq='W-MON')]
+    ).aggregate(
+        {"sentiment_tweet": "mean"}
+    ).rename(columns={"sentiment_tweet": f"sentiment_tweet for {gics_sector}"})
+
     plot_df = plot_df.merge(
         plot_df_sector, left_index=True, right_index=True,
         suffixes=(f" for {selected_company}", f" for {gics_sector}")
+        ).merge(
+            tweet_df,  left_index=True, right_index=True,
         )
     return plot_df, filtered_df.iloc[0]["symbol"]
 
@@ -142,12 +187,20 @@ app.layout = create_layout(
 
 @app.callback(
     Output('keyword_display', 'figure'),
+    Output('keyword_display', 'style'),
     Output('count_display', 'figure'),
+    Output('count_display', 'style'),
     Input('demo-dropdown', 'value'))
-def update_keywords(selected_company):
+def update_keywords(selected_company: str):
+    """
+    Update keyword and count graph
 
+    Parameters
+    ----------
+    selected_company: str
+    """
     if selected_company == "":
-        return fig_placeholder, fig_placeholder
+        return fig_placeholder, {"display": "none"}, fig_placeholder, {"display": "none"}
 
     keyword_df, count_df = calc_count_frames(selected_company)
 
@@ -169,16 +222,28 @@ def update_keywords(selected_company):
     )
     fig2.update_layout(transition_duration=500)
 
-    return fig, fig2
+    return fig, {}, fig2, {}
 
 
 @app.callback(
     Output('graph-1', 'figure'),
+    Output('graph-1', 'style'),
     Input('demo-dropdown', 'value'))
-def update_figure(selected_company):
+def update_figure(selected_company: str):
+    """
+    Update plotly time series
 
+    Parameters
+    ----------
+    selected_company : str
+        Company name
+
+    Returns
+    -------
+        plotly figure
+    """
     if selected_company == "":
-        return fig_placeholder
+        return fig_placeholder, {"display": "none"}
 
     plot_df, ticker_symbol = calc_sentiment_frame(selected_company)
     plot_df = add_ticker_frame(plot_df, ticker_symbol)
@@ -194,8 +259,18 @@ def update_figure(selected_company):
     fig.update_traces(connectgaps=True)
     fig.update_traces(mode='lines+markers')
     fig.update_layout(transition_duration=500)
+    return fig, {}
 
-    return fig
+
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def open_canvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
 
 
 if __name__ == '__main__':
--- code/webapp/layout.py
+++ code/webapp/layout.py
@@ -8,11 +8,34 @@ def create_layout(**kwargs):
     dropdown_options = kwargs["dropdown_options"]
 
     layout = html.Div(children=[
-        html.H1(children='Enigma'),
+        html.H1(children='Enigma',  style={"margin": "10px"}),
 
         html.Div(children='''
             Decoding industry sentiment
-        '''),
+        ''',  style={"margin": "10px"}),
+        dbc.Button(
+            "Show Info",
+            id="open-offcanvas", n_clicks=0, style={"margin": "20px"}
+        ),
+        dbc.Offcanvas(
+            [html.B("This app is still in early development", style={"margin-bottom": "20px"}),
+            html.P(
+                """
+                Data has been gathered from twitter and various online news sources.
+                Sentiment scores (0-1) have been calculated based on a stochasticd gradient 
+                classifier (logistic modell) 
+                which in turn used a TF-IDF Vectorizer as feature extractor.
+                By using the dropdown menu,  one can search for a company and display 
+                all infos regarding news and tweet sentiment.
+                """, 
+                style={"margin-bottom": "20px"}
+            ),
+            html.A("Github Repo", href='https://github.com/thoemi123/enigma', target="_blank")
+            ],
+            id="offcanvas",
+            title="Decoding industry sentiment",
+            is_open=False,
+        ),
         dbc.Row(
             dbc.Col(
                 html.Div(
@@ -23,15 +46,15 @@ def create_layout(**kwargs):
                             value='',
                             style={"color": "#303030"}
                         ),
-                        dcc.Graph(id='graph-1')
+                        dcc.Graph(id='graph-1', style={"display": "none"})
                     ]
                 )
             )
         ),
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id="keyword_display")),
-                dbc.Col(dcc.Graph(id="count_display")),
+                dbc.Col(dcc.Graph(id="keyword_display", style={"display": "none"})),
+                dbc.Col(dcc.Graph(id="count_display", style={"display": "none"})),
             ]
         )
     ])
@@ -41,6 +64,7 @@ def create_layout(**kwargs):
 
 fig_placeholder = {
     "layout": {
+        "paper_bgcolor": "#222",
         "xaxis": {
             "visible": False
         },
